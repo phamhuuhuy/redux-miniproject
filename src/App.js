70b6d9bb4561d80ce092b56e70d1a80093ff5853
Huu Huy <s3817719@rmit.edu.vn>
2021-07-14 22:09:06 +0700

    api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React, { Suspense } from 'react';
+
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header';
 import NotFound from './components/NotFound';
+import productApi from 'api/productApi';
 
 // Lazy load - Code splitting
 const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
+
+  const [productList, setProductList] = useState([])
+
+  useEffect(() => {
+
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          _page: 1,
+          _limit: 10,
+        }
+        const response = await productApi.getAll(params);
+        setProductList(response.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchProductList()
+  }, [])
+
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ...</div>}>
